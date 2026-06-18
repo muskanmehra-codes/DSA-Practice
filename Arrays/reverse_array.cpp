1c43b2cb47f78fe83160d5ad997c1f1b50abbe7a
Muskan <muskanmehra054@gmail.com>
2026-06-18 23:25:32 +0530

    Create reverse_array.cpp

---
+++ Arrays/reverse_array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    cout << "Original array: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    cout << "Reverse array: ";
+
+    for(int i = n-1; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
